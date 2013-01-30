1cc0e7fb3160759f6e82d5813d38b33e195c97f0
garrynewman <garrynewman@.(none)>
2013-01-30 12:40:08 +0000

    Very simple image loading

--- include/Bootil/Image/Image.h
+++ include/Bootil/Image/Image.h
@@ -4,7 +4,13 @@ namespace Bootil
 {
 	namespace Image
 	{
-
+		struct Format
+		{
+			unsigned int	width;
+			unsigned int	height;
+			AutoBuffer		data;
+			bool			alpha;
+		};
 	}
 
 }
--- src/Bootil/Image/JPEG.cpp
+++ src/Bootil/Image/JPEG.cpp
@@ -3,13 +3,35 @@
 
 #pragma once
 
+#define STBI_NO_STDIO
+#include "stb_image/stb_image.h"
+
 namespace Bootil 
 {
 	namespace Image
 	{
 		namespace JPEG 
 		{
+			BOOTIL_EXPORT bool Load( Bootil::Buffer& buf, Bootil::Image::Format& imageout )
+			{
+				stbi s;
+				start_mem( &s, (const uint8*) buf.GetBase(), buf.GetWritten() );
 
+				if ( !stbi_jpeg_test( &s ) ) 
+					return false;
+				
+				int x, y, comp;
+
+				unsigned char* pData = stbi_jpeg_load( &s, &x, &y, &comp, 3 );
+
+				imageout.width	= x;
+				imageout.height = y;
+				imageout.alpha	= false;
+				imageout.data.Write( pData, x * y * 3 );
+
+				stbi_image_free( pData );
+				return true;
+			}
 		}
 	}
 
