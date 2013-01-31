22b2a0663d48d9fd79aefc10882a2cd8a50d178d
garrynewman <garrynewman@.(none)>
2013-01-31 11:42:16 +0000

    Console library, allows for text colouring Debug::SuppressPopups to stop popups on errors etc (console apps)

---
+++ include/Bootil/Console/Console.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace Bootil 
+{
+	namespace Console
+	{
+		enum ConsoleColor
+		{
+			White,
+			Black,
+			Grey,
+			Red,
+			Blue,
+			Green,
+			Yellow
+		};
+
+		BOOTIL_EXPORT void FGColorPush( ConsoleColor col );
+		BOOTIL_EXPORT void FGColorPop();
+
+		BOOTIL_EXPORT void BGColorPush( ConsoleColor col );
+		BOOTIL_EXPORT void BGColorPop();
+
+		BOOTIL_EXPORT char WaitForKey();
+	}
+}
