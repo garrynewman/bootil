13a0dce47c572999e56fa2a713d768295e281946
garrynewman <garrynewman@gmail.com>
2013-07-01 08:09:03 -0700

    Linux/Mac configs - starter

--- src/3rdParty/curl/lib/curl_setup.h
+++ src/3rdParty/curl/lib/curl_setup.h
@@ -36,47 +36,17 @@
  * configuration file for platforms which lack config tool.
  */
 
-#ifdef HAVE_CONFIG_H
-
-#include "curl_config.h"
-
-#else /* HAVE_CONFIG_H */
-
-#ifdef _WIN32_WCE
-#  include "config-win32ce.h"
-#else
 #  ifdef WIN32
 #    include "config-win32.h"
 #  endif
-#endif
 
-#if defined(macintosh) && defined(__MRC__)
-#  include "config-mac.h"
-#endif
+# if defined(__APPLE__) && defined( __MACH__ )
+#    include "config-osx.h"
+#  endif
 
-#ifdef __riscos__
-#  include "config-riscos.h"
-#endif
-
-#ifdef __AMIGA__
-#  include "config-amigaos.h"
-#endif
-
-#ifdef __SYMBIAN32__
-#  include "config-symbian.h"
-#endif
-
-#ifdef __OS400__
-#  include "config-os400.h"
-#endif
-
-#ifdef TPF
-#  include "config-tpf.h"
-#endif
-
-#ifdef __VXWORKS__
-#  include "config-vxworks.h"
-#endif
+# ifdef __linux__
+#    include "config-linux.h"
+#  endif
 
 #endif /* HAVE_CONFIG_H */
 
