72d5fe1ea92add6af495b2550b5051292237dbba
garrynewman <garrynewman@.(none)>
2013-07-01 17:09:02 +0100

    include cc files too

--- projects/premake4.lua
+++ projects/premake4.lua
@@ -33,13 +33,13 @@ configuration "Debug"
 project "Bootil-Shared"
 	uuid ( "22348F3E-2CE7-42fd-B7DC-2861A17D9FEE" )
 	defines { "BOOTIL_COMPILE_DLL" }
-	files { "../src/**.cpp", "../src/**.c", "../include/**.c", "../include/**.cpp" }
+	files { "../src/**.cpp", "../src/**.c", "../src/**.cc" }
 	kind "SharedLib"
 	targetname( "bootil" )
 
 project "Bootil-Static"
 	uuid ( "AB8E7B19-A70C-4767-88DE-F02160167C2E" )
 	defines { "BOOTIL_COMPILE_STATIC" }
-	files { "../src/**.cpp", "../src/**.c", "../include/**.c", "../include/**.cpp" }
+	files { "../src/**.cpp", "../src/**.c", "../src/**.cc" }
 	kind "StaticLib"
 	targetname( "bootil_static" )
