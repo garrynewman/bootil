95ed6b0b9c0d0493615b533fcd330a3362d837f9
garrynewman <garrynewman@.(none)>
2013-07-16 08:59:07 +0100

    Cleaned up Init/Shutdown

--- src/Bootil/Base.cpp
+++ src/Bootil/Base.cpp
@@ -2,24 +2,16 @@
 
 namespace Bootil
 {
-	//LONG WINAPI BootilCrashHandler( PEXCEPTION_POINTERS pExceptionInfo );
-
 	static bool g_bShuttingDown = false;
 
 	BOOTIL_EXPORT void Startup()
 	{
-		//Bootil::Threads::Internal::Initialize();
-		//timeBeginPeriod( 1 );
-		//SetUnhandledExceptionFilter( BootilCrashHandler );
+		g_bShuttingDown = false;
 	}
 
 	BOOTIL_EXPORT void Shutdown()
 	{
 		g_bShuttingDown = true;
-		Output::Msg( "Exiting..\n" );
-		//Interface::UnloadAll();
-		//File::Shutdown();
-		Output::Msg( "Exited\n" );
 	}
 
 	BOOTIL_EXPORT bool IsShuttingDown()
