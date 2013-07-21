43346ec8859df9dee037023dda027a802e13e76b
garrynewman <garrynewman@.(none)>
2013-07-21 12:05:48 +0100

    Reading

--- include/Bootil/Network/Socket.h
+++ include/Bootil/Network/Socket.h
@@ -10,15 +10,31 @@ namespace Bootil
 
 				Socket();
 
-				// Listener
+				//
+				// Listener Mode (Start as a server)
+				//
 				bool InitAsListener( unsigned int iPort );
 				Socket* Accept();
 
+
+
+				// Returns true if connected
+				bool IsConnected();
+
+				// Closes the socket, ends all connections
 				void Close();
+
+
+				//
+				// Reading
+				//
+				unsigned long ReadLength();
+				unsigned long Read( void* pData, unsigned long iDataLen );
 				
 			private:
 
 				void InitializeSocket();
+				bool PreventedBlock();
 
 			protected:
 
