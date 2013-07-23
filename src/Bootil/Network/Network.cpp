2989c18d6e69df83bcf11b1d2a71f5beeeb212ec
garrynewman <garrynewman@.(none)>
2013-07-23 11:14:38 +0100

    Moved HTTP under the Network namespace

---
+++ src/Bootil/Network/Network.cpp
@@ -0,0 +1,29 @@
+
+#include "Bootil/Bootil.h"
+
+#ifdef _WIN32 
+	#include <winsock2.h>
+#endif 
+
+namespace Bootil
+{
+	namespace Network
+	{
+		BOOTIL_EXPORT void Start()
+		{
+			#ifdef _WIN32
+			WSAData wsaData;
+			WSAStartup( MAKEWORD(1, 1), &wsaData );
+			#endif 
+		}
+
+		BOOTIL_EXPORT void End()
+		{
+			#ifdef _WIN32
+
+			WSACleanup();
+			
+			#endif 
+		}
+	}
+}
