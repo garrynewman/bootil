0d5d63f68a4b99ca0ffb85d896a0254c322b9b35
garrynewman <garrynewman@gmail.com>
2013-12-14 03:31:15 -0800

    Quick Timestamp functions

--- include/Bootil/Utility/Time.h
+++ include/Bootil/Utility/Time.h
@@ -7,6 +7,9 @@ namespace Bootil
 		BOOTIL_EXPORT unsigned long long	UnixTimestamp();
 		BOOTIL_EXPORT Bootil::BString		UnixTimestampAsString();
 
+		BOOTIL_EXPORT Bootil::BString		TimeStamp(); // returns "23:45"
+		BOOTIL_EXPORT Bootil::BString		TimeAndDateStamp(); // returns "1982-05-20 23:45"
+
 		BOOTIL_EXPORT float Seconds();
 		BOOTIL_EXPORT float MilliSeconds();
 
