af3011348259bd2457e7403db797ddf19ee91174
garrynewman <garrynewman@gmail.com>
2014-02-01 06:19:11 -0800

    PNG loading JPEG saving

--- include/Bootil/Image/PNG.h
+++ include/Bootil/Image/PNG.h
@@ -4,10 +4,9 @@ namespace Bootil
 {
 	namespace Image
 	{
-		namespace JPEG
+		namespace PNG
 		{
 			BOOTIL_EXPORT bool Load( Bootil::Buffer & input, Bootil::Image::Format & output );
-			BOOTIL_EXPORT bool Save( Bootil::Image::Format & input, Bootil::Buffer & output, int iQuality = 85 );
 		}
 	}
 
